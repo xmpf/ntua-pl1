6f0acba22ea4e1f63d80b610db5fbbfa7ae8bf8c
Michalis Papadopoullos <mechatr0nic.cy@gmail.com>
2017-04-24 04:09:34 +0300

    first exercise

---
+++ Άσκηση 1/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+
+#define DEBUG(X) printf("\t {%lld, %lld} \t", X.value, X.index)
+#define DEBUG2(X) printf("\t [%lld] \t", X)
+
+/* Function prototypes */
+void showCall(char *);
+typedef long long int bigInt;
+
+/** main **/
+int main(int argc, char *argv[]) {
+
+    /********[ Input file ]************/
+    FILE *fin = fopen ("file.in", "r");
+
+    /********[ Data structures]********/
+    typedef struct {
+        bigInt value;
+        bigInt index;
+    } pair_t;
+    pair_t *L;
+    pair_t *R;
+
+    /********[ Parse input ]***********/
+    bigInt n, // number of stops
+             i = 0, j = 0, // index
+            *A; // array of heights
+
+    fscanf (fin, " %lld", &n);
+
+    A = new bigInt[n]; // allocate space for heights
+    L = new pair_t[n];
+    R = new pair_t[n];
+
+    // parse-from-file loop
+    while (i < n) { // while not EOF encountered :: O(n)
+        fscanf (fin, " %lld", &A[i]);
+        i++;
+    }
+
+    /*******[ Processing ]*****/
+    // A: array of all heights
+    // L: array of <value, index> from LEFT -> RIGHT
+    // ... current < previous
+    // R: array of <value, index> from RIGHT -> LEFT
+    // ... current > previous
+    pair_t l, r;
+    l.value = A[0];
+    l.index = 1;
+    L[0] = l;
+
+    r.value = A[n - 1];
+    r.index = n;
+    R[0] = r;
+
+    bigInt rLim = 0, lLim = 0;
+
+    // LEFT -> RIGHT traverse
+    j = 1;
+    for (i = 1; i < n; i++) { // populate L list :: O(n)
+        if (l.value > A[i]) {
+            l.value = A[i];
+            l.index = i + 1;
+            L[j++] = l;
+        }
+        lLim = j;
+    }
+    // RIGHT -> LEFT traverse
+    j = 1;
+    for (i = n - 2; i >= 0; i--) { // populate R list :: O(n)
+        if ((r.value) < A[i]) {
+            r.value = A[i];
+            r.index = i + 1;
+            R[j++] = r;
+        }
+        rLim = j;
+    }
+
+    // return max (|Jindex - Iindex|) forall 0 < i,j < N
+    bigInt max = 0, length = 0;
+
+    for (i = 0; i < lLim; i++) {
+        for (j = rLim - 1; L[i].value < R[j].value; j--) {}
+        length = R[j + 1].index - L[i].index;
+        if (max < length) { max = length; }
+        i++;
+    }
+    printf ("\nResult: %lld\n", max);
+    /*****[ Clean-Up routine ]*****/
+    delete[] A; // free allocated space
+    A = NULL;   // point A to NULL
+    delete[] L;
+    L = NULL;
+    delete[] R;
+    R = NULL;
+    fclose (fin);
+    return 0;
+} // end-of-main
+
+void showCall (char *s)
+{
+    printf ("Usage:\t %s <in-file>\n", s);
+}
